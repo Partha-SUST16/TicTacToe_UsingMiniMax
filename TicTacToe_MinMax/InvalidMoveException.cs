bf1487a60ce79ba3175c6d0ebeb225b3917e89ba
Partha-SUST16 <partha05@student.sust.edu>
2020-11-12 00:21:10 +0600

    Board class created

---
+++ TicTacToe_MinMax/InvalidMoveException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TicTacToe_MinMax
+{
+    /// <summary>
+    /// An Exception representing an invalid move
+    /// </summary>
+    public class InvalidMoveException : Exception
+    {
+        public InvalidMoveException()
+            : base()
+        {
+        }
+
+        public InvalidMoveException(string msg)
+            : base(msg)
+        {
+        }
+    }
+}
