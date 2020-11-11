5eb8471f41119d852c369adf7eb50bbc185dd523
Partha-SUST16 <partha05@student.sust.edu>
2020-11-11 23:40:21 +0600

    Initial Commit

---
+++ TicTacToe_MinMax/Board.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe_MinMax
+{
+    /// <summary>
+    /// This class represents a tic-tac-toe board
+    /// It is Cloneable so that we can copy board configurations when searching for a move
+    /// </summary>
+    public class Board : ICloneable
+    {
+        public enum Pieces { X, O, Empty };
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
